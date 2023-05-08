8514e1cf1ae8419e79da43696e2e8608917ac920
Nischal Puri <mrnisal2.np@gmail.com>
2023-05-08 20:13:15 +0545

    install tailwind css

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <main className={styles.main}> */}
-      <h>NEXT JS</h>
-      {/* </main> */}
+      <main>
+        <h1 className="text-red-400">home</h1>
+      </main>
     </>
   )
 }
